a3dc1b789824a3ab6c30aee86e421bb9dd7f702d
sivanshu_sinha <sivanshusinha8930@gmail.com>
2026-02-21 19:25:24 +0530

    added a file compressor project

---
+++ File Compressor/file_compressor.py
@@ -0,0 +1,60 @@
+import FreeSimpleGUI as sg
+from zip_maker import zipCreate
+
+layout = [
+
+    [sg.Text("Select Files to Compress", font=("Arial", 12, "bold"))],
+
+    [
+        sg.Button("Choose Files", key="browse_files"),
+        sg.Input(size=(40,1), key="file")
+    ],
+
+    [sg.Text("Select Destination Folder", font=("Arial", 12, "bold"))],
+
+    [
+        sg.Button("Choose Folder", key="browse_folder"),
+        sg.Input(size=(40,1), key="folder")
+    ],
+
+    [sg.Push(), sg.Button("Compress", size=(15,1)), sg.Push()]
+]
+
+window = sg.Window("File Compressor", layout)
+
+while True:
+    event, values = window.read()
+    print(event,values)
+
+    if event == sg.WINDOW_CLOSED:
+        break
+
+    if event == "browse_files":
+        files = sg.popup_get_file("Select Files")
+
+        if files:
+            window["file"].update(files)
+
+    if event == "browse_folder":
+        folder = sg.popup_get_folder("Select Folder")
+        if folder:
+            window["folder"].update(folder)
+
+    if event == "Compress":
+
+        if not values["file"]:
+            sg.popup("Please select files first!")
+            continue
+
+        if not values["folder"]:
+            sg.popup("Please select destination folder!")
+            continue
+
+        filePaths = values["file"].split(";")
+        folder = values["folder"]
+
+        zipCreate(filePaths, folder)
+
+        sg.popup("Compression Successful!")
+
+window.close()
---
+++ File Compressor/zip_maker.py
@@ -0,0 +1,20 @@
+import pathlib
+import zipfile
+
+#filePaths : files that needs to be compressed
+#destDir : Location where the compressed file will save
+def zipCreate(filePaths, destDir): 
+    try:
+        dest = pathlib.Path(destDir, 'compressed.zip') # this means in location destDir, the compressed file will be saved as compressed.zip
+        #dest has only address not zip file, till now zip file is not created
+
+        with zipfile.ZipFile(dest, 'w') as archive:
+            for file_path in filePaths:
+                file_path = pathlib.Path(file_path) # this line will convert the filepath(that was initially in string) to proper path
+                archive.write(file_path, arcname=file_path.name) # this line means put the files to archive 
+                # imagine archive as a box in which we will put the compressed files into
+                # arcname means what will be the name of the compressed file in that archive folder
+                # arcname is basically used to give the name to compressed file in that folder
+
+    except Exception as e:
+        print("Error:", e)
