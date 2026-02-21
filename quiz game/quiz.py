a0e4a5943f2fd2f239e17c1c9fc011ada05200db
sivanshu_sinha <sivanshusinha8930@gmail.com>
2026-02-21 19:22:24 +0530

    added a quize game through json

---
+++ quiz game/quiz.py
@@ -0,0 +1,27 @@
+import json
+
+# Load questions from the json file
+with open("questions.json","r") as file:
+    data = json.load(file)
+
+score = 0
+question = data["questions"]
+
+for q in question :
+    print(f"\nQ{q["id"]}.{q["question"]}")
+
+    for key,value in q["options"].items():
+        print(f'{key}.{value}')
+
+    user_ans = int(input("Enter your answer"))
+
+    if user_ans == q["correct_answer"]:
+        print("✅ Correct")
+        score += 1
+    else:
+        print("❌ Wrong")
+        print(f"Correct answer was option {q['correct_answer']}")
+
+# Final score
+print(f"\nYour final score: {score}/{len(question)}")
+
